2521e5fb55fd74f24d50e0605ff55f7a5544cc28
Mahfuj Hussain <mahfujhussain284@gmail.com>
2025-12-17 14:41:48 +0600

    done signup design.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ticketmaster/pages/home.dart';
+import 'package:ticketmaster/pages/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Home(),
+      home: Signup(),
     );
   }
 }
---
+++ lib/pages/signup.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({Key? key}) : super(key: key);
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Column(
+          children: [
+            Image.asset('assets/images/onboarding.png'),
+            SizedBox(height: 10),
+            Text(
+              "Unlock the features of",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            Text(
+              "Event Booking App",
+              style: TextStyle(
+                color: Color(0xff6351ec),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Discover, book, and experience unforgettable movements effectlessly!",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black45, fontSize: 20),
+            ),
+            SizedBox(height: 50),
+            Container(
+              height: 70,
+              margin: EdgeInsets.only(left: 30, right: 30),
+              decoration: BoxDecoration(
+                color: Color(0xff6351ec),
+                borderRadius: BorderRadius.circular(45),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/google.png',
+                    height: 30,
+                    width: 30,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    "Sign in with Google",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
