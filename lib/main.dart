3329e5154055bdca508b2e21490e47d9599da89d
Mahfuj Hussain <mahfujhussain284@gmail.com>
2025-12-29 14:29:55 +0600

    DOne

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: BottomNav(), // unchanged
+      home: Signup(), // unchanged
     );
   }
 }
